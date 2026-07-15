29b768b72f45421856689c78a54e181d95ed92cd
Wonil Choi <wonil4198@gmail.com>
2026-07-15 15:49:24 +0900

    2026-07-15

--- spring-board/src/test/java/net/likelion/bebc25/board01/SpringBoard03ApplicationTests.java
+++ spring-board/src/test/java/net/likelion/bebc25/board01/SpringBoard03ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBoard02ApplicationTests {
+class SpringBoard03ApplicationTests {
 
     @Test
     void contextLoads() {
